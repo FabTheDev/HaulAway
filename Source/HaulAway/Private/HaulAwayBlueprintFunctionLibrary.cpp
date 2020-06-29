d2891abd9d1e885edb57f93dad2405a40fdb2e48
FabTheDev <fabriceham@hotmail.fr>
2020-06-29 02:50:48 +0200

    Iceberg tilled : génération fonctionelle

---
+++ Source/HaulAway/Private/HaulAwayBlueprintFunctionLibrary.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HaulAwayBlueprintFunctionLibrary.h"
+
+
+
+void UHaulAwayBlueprintFunctionLibrary::Editor_MoveActorToOutlinerFolder(AActor* Target, const FName & NewFolderPath)
+{
+	if (Target != nullptr)
+	{
+		#if WITH_EDITOR
+		Target->SetFolderPath(NewFolderPath);
+		#endif
+	}
+	
+}
---
+++ Source/HaulAway/Public/HaulAwayBlueprintFunctionLibrary.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "GameFramework/Actor.h"
+#include "HaulAwayBlueprintFunctionLibrary.generated.h"
+
+
+/**
+ * 
+ */
+
+UCLASS()
+class HAULAWAY_API UHaulAwayBlueprintFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	UFUNCTION(BlueprintCallable, Category = "Actor| Editor")
+	static void Editor_MoveActorToOutlinerFolder(AActor* Target, const FName& NewFolderPath);
+};
